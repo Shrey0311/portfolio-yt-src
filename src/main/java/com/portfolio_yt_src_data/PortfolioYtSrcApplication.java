1c6cf5ad781b2ad8154cc5b9a8397984cf0aa166
Shrey Kulshrestha <68810677+Shrey0311@users.noreply.github.com>
2025-03-04 21:17:35 +0530

    Initial commit
    Portfolio Source Code

---
+++ src/main/java/com/portfolio_yt_src_data/PortfolioYtSrcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio_yt_src_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioYtSrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioYtSrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio_yt_src_data/PortfolioYtSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio_yt_src_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioYtSrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
